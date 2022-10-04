dd7855d87c07197269fbdec441e98807ac336de9
AlekseiAgik <108349099+AlekseiAgik@users.noreply.github.com>
2022-10-04 19:21:03 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,24 @@
+"""blogger_srv URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-from blog_app.views import index, page1, page2, page, article_show_all
 
 
 urlpatterns = [
-    path('', index),
-    path('index/', index),
-    path('page1/', page1, name='page1'),
-    path('page2/', page2, name='page2'),
-    path('page/<int:num_page>', page, name='page'),
-    path('article_all/', article_show_all, name='article_show_all'),
-
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog_app.urls')),
+    path('', include('blog_app.urls'))
 ]
-
-
